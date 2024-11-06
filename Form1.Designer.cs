dbeb88286a0f03ffe7b902e3cd7e6be33866bc98
RicardoRiberaR <ricardoribera156@gmail.com>
2024-11-06 11:59:27 -0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Venta360
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TBcorreo = new System.Windows.Forms.TextBox();
+            this.TBcontraseña = new System.Windows.Forms.TextBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(274, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CORREO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(274, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CONTRASEÑA";
+            // 
+            // TBcorreo
+            // 
+            this.TBcorreo.Location = new System.Drawing.Point(277, 167);
+            this.TBcorreo.Name = "TBcorreo";
+            this.TBcorreo.Size = new System.Drawing.Size(206, 20);
+            this.TBcorreo.TabIndex = 2;
+            // 
+            // TBcontraseña
+            // 
+            this.TBcontraseña.Location = new System.Drawing.Point(277, 233);
+            this.TBcontraseña.Name = "TBcontraseña";
+            this.TBcontraseña.Size = new System.Drawing.Size(206, 20);
+            this.TBcontraseña.TabIndex = 3;
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Location = new System.Drawing.Point(294, 335);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(175, 32);
+            this.btnEntrar.TabIndex = 4;
+            this.btnEntrar.Text = "ENTRAR";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(287, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(196, 39);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "REGISTRO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.TBcontraseña);
+            this.Controls.Add(this.TBcorreo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TBcorreo;
+        private System.Windows.Forms.TextBox TBcontraseña;
+        private System.Windows.Forms.Button btnEntrar;
+        public System.Windows.Forms.Label label3;
+    }
+}
+
